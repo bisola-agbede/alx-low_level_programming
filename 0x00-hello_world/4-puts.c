180fc3ef2e72ad7a107c16842de7fe032f188558
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-02 19:54:35 -0700

    FIX

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-/** 
+/**
  * main -Entry point
  * Return: Always 0 (success)
  */
@@ -7,4 +7,4 @@ int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
-} 
+}
