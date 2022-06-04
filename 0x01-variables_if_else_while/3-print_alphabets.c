ef5341d8fb225f6532fb5617163e9a03c737b4fa
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-03 18:00:31 -0700

    create 3 print alphabets file

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - Prints all alphabets in lower case and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x = 'a';
+	int y = 'A';
+
+	while (x <= 'z')
+	{
+		putchar(x);
+		x++;
+	}
+	while (y <= 'Z')
+	{
+		putchar(y);
+		y++;
+	}
+	putchar('\n');
+	return (0);
+}
