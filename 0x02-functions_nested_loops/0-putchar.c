ddea991dcd100027ac8ab137c00b955f7f8877ed
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-07 15:28:37 -0700

    Fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * Main - a program that prints _putchar
- * followed by a new line
+ * main - a program that prints _putchar
+ * 
  * Return: Always 0 (success)
  */
 int main(void)
