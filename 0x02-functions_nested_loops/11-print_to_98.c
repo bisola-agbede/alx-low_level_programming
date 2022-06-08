63d6476a329cb15512239665c685506ea02a9606
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-08 14:03:20 -0700

    Fix

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,19 +9,17 @@
  */
 void print_to_98(int n)
 {
-	if (n > 98)
+	while (n < 98)
 	{
-		for (; n > 98; n--)
-		{
-			printf(%d, ", n);
-		}
+		printf("%d, ", n);
+		n++;
 	}
-	else if (n < 98)
+	while (n > 98)
 	{
-		for (; n < 98; n++)
-		{
-			printf("%d, ", n);
-		}
+		printf("%d, ", n);
+		n--;
 	}
-	printf("%d\n", n);
-}
+	if (n == 98)
+		printf("%d", n);
+	printf("\n");
+}}
