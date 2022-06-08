a1c49f60052ab0dfa86ecdf862d61710aa600563
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-08 12:37:10 -0700

    create 10 add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * add - adds two intergers and return the value
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return int
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
