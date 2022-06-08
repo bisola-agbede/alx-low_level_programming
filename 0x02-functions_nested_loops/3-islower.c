e322fa766e9b13f97d864f6790774fa7a2a39c68
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-07 18:03:13 -0700

    create 3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _islower - program that checks for lowercase character
+ *
+ * Return 1 if lowercase, otherwise return 0
+ */
+int _islower(int c)
+{
+	int i = islower(c);
+
+	if (i < z)
+	{
+		return (1);
+	}
+	else 
+	{
+		return (0)
+	}
+}
