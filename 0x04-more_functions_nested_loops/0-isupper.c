e4c5f55d37051d42a7dbda891423e2c2f61ce3df
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-09 17:26:34 -0700

    create 0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _isupper - checks for an uppercase character
+ *
+ * @c: variable to check if uppercase
+ *
+ * Return: 1 if uppercase, 0 otherwise
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
