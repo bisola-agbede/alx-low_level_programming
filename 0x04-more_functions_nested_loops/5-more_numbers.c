de450d9658733997d2e4a95c3f2018def60d9ea9
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-10 12:24:52 -0700

    Fix

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,12 +9,16 @@ void more_numbers(void)
 	int i = 1;
 	int j;
 
-	while (i <= 10)
+	while (i < 10)
 	{
-		j = '0';
-		while (j <= '14')
+		j = 0;
+		while (j <= 14)
 		{
-			_putchar(j);
+			if (j >= 10)
+			{
+				_putchar(j / 10 + '0');
+			}
+			_putchar(j % 10 + '0');
 			j++;
 		}
 		_putchar('\n');
