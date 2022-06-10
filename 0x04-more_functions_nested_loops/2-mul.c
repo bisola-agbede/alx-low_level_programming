62ce8ca41886afe46e000f648e312ddc996881ed
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-09 17:51:58 -0700

    Create 2 mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - multiplies two intergers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: return int
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
