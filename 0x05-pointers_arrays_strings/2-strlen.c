6943a7bea72c0e0f708233941ced850769a7446f
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-13 19:08:49 -0700

    create strlen file

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strlen - returns the lenth of a string
+ *
+ * @s: A string to be tested
+ *
+ * Return: Always 0
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	_putchar(strlen(s));
+	return (0);
+}
