2926a89ed3b688179842eb557822b400a5046703
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-13 19:27:58 -0700

    print every character in order

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ *puts2 - prints every other character of a string
+ starting withbthe first character
+ **@str: pointer to char
+ */
+
+void puts2(char *str)
+{
+int i = 0;
+
+while (str[i] != '\0')
+{
+
+if (i % 2 == 0)
+
+{
+printf("%c", str[i]);
+}
+
+i++;
+}
+printf("\n");
+}
