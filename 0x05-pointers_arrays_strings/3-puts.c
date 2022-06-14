6ef6f42a315fd1f50c830f0effa1df3e21e77248
bisola-agbede <bisolaagbede01@gmail.com>
2022-06-13 19:17:31 -0700

    create puts file

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include<stdio.h>
+
+/**
+ * _puts - prints a string
+ * followed by a new line
+ * to stdout
+ **@s: pointer to char
+ */
+
+void _puts(char *s)
+{
+	puts(s);
+}
